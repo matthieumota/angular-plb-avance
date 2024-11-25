3afc998f14ddeea988455ec4cb4941198ee70e4b
Matthieu Mota <matthieumota@gmail.com>
2024-11-25 11:36:50 +0100

    Prepare housing service

---
+++ src/app/services/housing.service.ts
@@ -0,0 +1,9 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HousingService {
+  constructor(private http: HttpClient) { }
+}
