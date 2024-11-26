d936661cecd63755a4b6a873c7eeef1c63da04f7
Matthieu Mota <matthieumota@gmail.com>
2024-11-26 17:06:15 +0100

    Show detail house

--- src/app/pages/house/house.component.ts
+++ src/app/pages/house/house.component.ts
@@ -1,17 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 import { HousingService } from '../../services/housing.service';
-import { switchMap } from 'rxjs';
+import { delay, switchMap } from 'rxjs';
 import { House } from '../../models/house';
+import { TaxPipe } from '../../pipes/tax.pipe';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-house',
-  imports: [],
+  imports: [TaxPipe, CurrencyPipe, RouterLink],
   templateUrl: './house.component.html',
   styleUrl: './house.component.scss'
 })
 export class HouseComponent implements OnInit {
   house!: House;
+  loading: boolean = true;
 
   constructor(
     private route: ActivatedRoute,
@@ -20,7 +23,14 @@ export class HouseComponent implements OnInit {
 
   ngOnInit(): void {
     this.route.paramMap.pipe(
-      switchMap(paramMap => this.housing.get(parseInt(paramMap.get('id') || '')))
-    ).subscribe(house => this.house = house)
+      switchMap(paramMap => {
+        this.loading = true
+        return this.housing.get(parseInt(paramMap.get('id') || ''))
+      }),
+      delay(500)
+    ).subscribe(house => {
+      this.house = house
+      this.loading = false
+    })
   }
 }
