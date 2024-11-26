90207bc36d8b34cd1aebcf3bd39de92fa65bd10a
Matthieu Mota <matthieumota@gmail.com>
2024-11-26 09:57:19 +0100

    House card test

--- src/app/components/house-card/house-card.component.spec.ts
+++ src/app/components/house-card/house-card.component.spec.ts
@@ -1,6 +1,7 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { HouseCardComponent } from './house-card.component';
+import { FakeHousingService } from '../../services/fake-housing.service';
 
 describe('HouseCardComponent', () => {
   let component: HouseCardComponent;
@@ -14,10 +15,13 @@ describe('HouseCardComponent', () => {
 
     fixture = TestBed.createComponent(HouseCardComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  it('should create', waitForAsync(() => {
+    TestBed.inject(FakeHousingService).all().subscribe(houses => {
+      fixture.componentRef.setInput('house', houses[0]);
+      fixture.detectChanges();
+      expect(component.house.name).toBe('Charming Studio');
+    });
+  }));
 });
--- src/app/pages/houses/houses.component.ts
+++ src/app/pages/houses/houses.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
 import { House } from '../../models/house';
 import { HousingService } from '../../services/housing.service';
 import { HouseCardComponent } from "../../components/house-card/house-card.component";
@@ -13,9 +13,18 @@ import { FormsModule } from '@angular/forms';
 export class HousesComponent {
   houses!: House[];
 
-  constructor(private housing: HousingService) {}
+  constructor(private housing: HousingService, private zone: NgZone) {}
 
   ngOnInit(): void {
     this.housing.all().subscribe(houses => this.houses = houses)
+
+    this.zone.runOutsideAngular(() => {
+      window.addEventListener('scroll', (event) => {
+        if (window.scrollY + window.innerHeight > document.body.scrollHeight) {
+          console.log(event)
+          this.zone.run(() => this.houses = this.houses.concat(this.houses))
+        }
+      })
+    })
   }
 }
