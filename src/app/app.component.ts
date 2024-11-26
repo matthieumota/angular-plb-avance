1ab04233b4e8649173bf77599fbbc11fd14292b2
Matthieu Mota <matthieumota@gmail.com>
2024-11-26 13:57:16 +0100

    Style header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { DsfrHeaderModule } from '@edugouvfr/ngx-dsfr';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, DsfrHeaderModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
