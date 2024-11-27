e9d411e710a1d4e691a98b587eef23753985de76
Matthieu Mota <matthieumota@gmail.com>
2024-11-27 12:05:09 +0100

    Use store

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
+import { AsyncPipe, JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { DsfrHeaderModule } from '@edugouvfr/ngx-dsfr';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, DsfrHeaderModule],
+  imports: [RouterOutlet, RouterLink, DsfrHeaderModule, AsyncPipe, JsonPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'Immobiliare';
+  cart$!: Observable<any[]>
+
+  constructor(private store: Store<{ cart: any[] }>) {
+    this.cart$ = this.store.select('cart')
+  }
 }
